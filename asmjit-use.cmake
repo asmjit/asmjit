9bd73646c82a6ca9b282baf67c2709bc08747d1c
xantares <xantares09@hotmail.com>
2014-08-19 07:29:02 +0000

    Added cmake config file.

---
+++ asmjit-use.cmake
@@ -0,0 +1,9 @@
+#                                               -*- cmake -*-
+#
+#  asmjit-use.cmake
+#
+
+add_definitions     ( ${ASMJIT_DEFINITIONS} )
+include_directories ( ${ASMJIT_INCLUDE_DIRS} )
+link_directories    ( ${ASMJIT_LIBRARY_DIRS} )
+
