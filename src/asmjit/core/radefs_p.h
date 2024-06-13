f5df7a2b1bd942fc1169d0f563dacbe34e6a11ac
kobalicek <kobalicek.petr@gmail.com>
2024-06-13 13:17:50 +0200

    Improved the performance of bin-packing (Compiler) (fixes #440)
    During bin-packing, a single function nonOverlappingUnionOf() is
    called many times to calculate a union of one live ranges with
    another. Before this change it used ZoneVector::reserve() to make
    sure that there is enough space for the union, however, in some
    cases this is not ideal in case that the union grows every time
    the function is called. In that case it's reallocating the vector
    many times, which affects performance.
    
    Instead of calling reserve(), a new function growingReserve() was
    added to tell the vector to grow when it needs to reallocate.
    
    In addition, this change fixes some documentation regarding the
    use of JitAllocator (Explicit Code Relocation section in core.h).

--- src/asmjit/core/radefs_p.h
+++ src/asmjit/core/radefs_p.h
@@ -559,7 +559,7 @@ public:
 
   ASMJIT_FORCE_INLINE Error nonOverlappingUnionOf(ZoneAllocator* allocator, const RALiveSpans<T>& x, const RALiveSpans<T>& y, const DataType& yData) noexcept {
     uint32_t finalSize = x.size() + y.size();
-    ASMJIT_PROPAGATE(_data.reserve(allocator, finalSize));
+    ASMJIT_PROPAGATE(_data.growingReserve(allocator, finalSize));
 
     T* dstPtr = _data.data();
     const T* xSpan = x.data();
@@ -694,7 +694,7 @@ typedef RALiveSpans<LiveRegSpan> LiveRegSpans;
 //!   - LEA x{  W|Out}, [x{R|Use} + y{R|Out}]  -> {x:R|W|Use|Out y:R|Use}
 //!
 //! It should be obvious from the example above how these flags get created. Each operand contains READ/WRITE
-//! information, which is then merged to RATiedReg's flags. However, we also need to represent the possitility
+//! information, which is then merged to RATiedReg's flags. However, we also need to represent the possibility
 //! to view the operation as two independent operations - USE and OUT, because the register allocator first
 //! allocates USE registers, and then assigns OUT registers independently of USE registers.
 enum class RATiedFlags : uint32_t {
