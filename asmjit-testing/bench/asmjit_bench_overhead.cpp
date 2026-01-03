f64c90818ff2ef87ec4f73f44d0a7e73fbff3229
kobalicek <kobalicek.petr@gmail.com>
2026-01-03 23:52:51 +0100

    Improved overhead benchmarks

--- asmjit-testing/bench/asmjit_bench_overhead.cpp
+++ asmjit-testing/bench/asmjit_bench_overhead.cpp
@@ -20,12 +20,15 @@ static void print_app_info(size_t n) noexcept {
   printf("This benchmark was designed to benchmark the cost of initialization and\n"
          "reset (or reinitialization) of CodeHolder and Emitters; and the cost of\n"
          "moving a minimal assembled function to executable memory. Each output line\n"
-         "uses \"<Test> [Func] [Finalize] [RT]\" format, with the following meaning:\n"
+         "provides the following columns:\n"
          "\n"
          "  - <Test>     - test case name - either 'CodeHolder' only or an emitter\n"
-         "  - [Func]     - function was assembled\n"
-         "  - [Finalize] - function was finalized (Builder/Compiler)\n"
-         "  - [RT]       - function was added to JitRuntime and then removed from it\n"
+         "  - Strategy   - reusability strategy - whether init/reset or reinit is used\n"
+         "  - Reuse Only - no code generation, no use of emitter except for init/reuse\n"
+         "  - Func       - function was assembled\n"
+         "  - RA         - function was compiled (registers allocated) (Compiler)\n"
+         "  - Asm        - function was finalized & serialized (Builder/Compiler)\n"
+         "  - RT         - function was added to JitRuntime and then removed from it\n"
          "\n"
          "Essentially the output provides an insight into the cost of reusing\n"
          "CodeHolder and other emitters, and the cost of assembling, finalizing,\n"
@@ -36,6 +39,24 @@ static void print_app_info(size_t n) noexcept {
   printf("The number of iterations benchmarked: %zu (override by --count=n)\n", n);
   printf("\n");
 }
+
+enum class AssemblerOp {
+  kNone,
+  kRT
+};
+
+enum class BuilderOp {
+  kNone,
+  kFinalize,
+  kFinalize_RT
+};
+
+enum class CompilerOp {
+  kNone,
+  kCompile,
+  kFinalize,
+  kFinalize_RT
+};
 
 #if !defined(ASMJIT_NO_JIT)
 
@@ -127,7 +148,7 @@ static inline void bench_assembler(InitStrategy strategy, size_t count) {
   }
 }
 
-template<typename AssemblerT>
+template<typename AssemblerT, AssemblerOp op>
 static inline void bench_assembler_func(InitStrategy strategy, size_t count) {
   JitRuntime rt;
   CodeHolder code;
@@ -140,40 +161,13 @@ static inline void bench_assembler_func(InitStrategy strategy, size_t count) {
       code.set_error_handler(&eh);
       code.attach(&a);
       emit_raw_func(a);
-      code.reset();
-    }
-  }
-  else {
-    code.init(rt.environment());
-    code.set_error_handler(&eh);
-    code.attach(&a);
 
-    for (size_t i = 0; i < count; i++) {
-      code.reinit();
-      emit_raw_func(a);
-    }
-  }
-}
-
-template<typename AssemblerT>
-static inline void bench_assembler_func_rt(InitStrategy strategy, size_t count) {
-  JitRuntime rt;
-  CodeHolder code;
-  AssemblerT a;
-  MyErrorHandler eh;
-
-  using Func = uint32_t(*)(void);
-
-  if (strategy == InitStrategy::kInitReset) {
-    for (size_t i = 0; i < count; i++) {
-      code.init(rt.environment());
-      code.set_error_handler(&eh);
-      code.attach(&a);
-      emit_raw_func(a);
-
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
+      if constexpr (op == AssemblerOp::kRT) {
+        using Func = uint32_t(*)(void);
+        Func fn;
+        rt.add(&fn, &code);
+        rt.release(fn);
+      }
 
       code.reset();
     }
@@ -187,9 +181,12 @@ static inline void bench_assembler_func_rt(InitStrategy strategy, size_t count) 
       code.reinit();
       emit_raw_func(a);
 
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
+      if constexpr (op == AssemblerOp::kRT) {
+        using Func = uint32_t(*)(void);
+        Func fn;
+        rt.add(&fn, &code);
+        rt.release(fn);
+      }
     }
   }
 }
@@ -222,8 +219,8 @@ static inline void bench_builder(InitStrategy strategy, size_t count) {
   }
 }
 
-template<typename BuilderT>
-static inline void bench_builder_func(InitStrategy strategy, size_t count, bool finalize) {
+template<typename BuilderT, BuilderOp op>
+static inline void bench_builder_func(InitStrategy strategy, size_t count) {
   JitRuntime rt;
   CodeHolder code;
   BuilderT b;
@@ -236,9 +233,16 @@ static inline void bench_builder_func(InitStrategy strategy, size_t count, bool 
       code.attach(&b);
       emit_raw_func(b);
 
-      if (finalize) {
+      if constexpr (op >= BuilderOp::kFinalize) {
         b.finalize();
+        if constexpr (op == BuilderOp::kFinalize_RT) {
+          using Func = uint32_t(*)(void);
+          Func fn;
+          rt.add(&fn, &code);
+          rt.release(fn);
+        }
       }
+
       code.reset();
     }
   }
@@ -251,50 +255,15 @@ static inline void bench_builder_func(InitStrategy strategy, size_t count, bool 
       code.reinit();
       emit_raw_func(b);
 
-      if (finalize) {
+      if constexpr (op >= BuilderOp::kFinalize) {
         b.finalize();
+        if constexpr (op == BuilderOp::kFinalize_RT) {
+          using Func = uint32_t(*)(void);
+          Func fn;
+          rt.add(&fn, &code);
+          rt.release(fn);
+        }
       }
-    }
-  }
-}
-
-template<typename BuilderT>
-static inline void bench_builder_func_finalize_rt(InitStrategy strategy, size_t count) {
-  JitRuntime rt;
-  CodeHolder code;
-  BuilderT b;
-  MyErrorHandler eh;
-
-  using Func = uint32_t(*)(void);
-
-  if (strategy == InitStrategy::kInitReset) {
-    for (size_t i = 0; i < count; i++) {
-      code.init(rt.environment());
-      code.set_error_handler(&eh);
-      code.attach(&b);
-      emit_raw_func(b);
-      b.finalize();
-
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
-
-      code.reset();
-    }
-  }
-  else {
-    code.init(rt.environment());
-    code.set_error_handler(&eh);
-    code.attach(&b);
-
-    for (size_t i = 0; i < count; i++) {
-      code.reinit();
-      emit_raw_func(b);
-      b.finalize();
-
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
     }
   }
 }
@@ -327,8 +296,8 @@ static inline void bench_compiler(InitStrategy strategy, size_t count) {
   }
 }
 
-template<typename CompilerT>
-static inline void bench_compiler_func(InitStrategy strategy, size_t count, bool finalize) {
+template<typename CompilerT, CompilerOp op>
+static inline void bench_compiler_func(InitStrategy strategy, size_t count) {
   JitRuntime rt;
   CodeHolder code;
   CompilerT cc;
@@ -344,8 +313,17 @@ static inline void bench_compiler_func(InitStrategy strategy, size_t count, bool
       compile_raw_func(cc);
       cc.end_func();
 
-      if (finalize) {
+      if constexpr (op == CompilerOp::kCompile) {
+        cc.run_passes();
+      }
+      else if constexpr (op >= CompilerOp::kFinalize) {
         cc.finalize();
+        if constexpr (op == CompilerOp::kFinalize_RT) {
+          using Func = uint32_t(*)(void);
+          Func fn;
+          rt.add(&fn, &code);
+          rt.release(fn);
+        }
       }
 
       code.reset();
@@ -363,99 +341,70 @@ static inline void bench_compiler_func(InitStrategy strategy, size_t count, bool
       compile_raw_func(cc);
       cc.end_func();
 
-      if (finalize) {
-        cc.finalize();
+      if constexpr (op == CompilerOp::kCompile) {
+        cc.run_passes();
       }
-    }
-  }
-}
-
-template<typename CompilerT>
-static inline void bench_compiler_func_rt(InitStrategy strategy, size_t count) {
-  JitRuntime rt;
-  CodeHolder code;
-  CompilerT cc;
-  MyErrorHandler eh;
-
-  using Func = uint32_t(*)(void);
-
-  if (strategy == InitStrategy::kInitReset) {
-    for (size_t i = 0; i < count; i++) {
-      code.init(rt.environment());
-      code.set_error_handler(&eh);
-      code.attach(&cc);
-
-      (void)cc.add_func(FuncSignature::build<uint32_t>());
-      compile_raw_func(cc);
-      cc.end_func();
-      cc.finalize();
-
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
-
-      code.reset();
-    }
-  }
-  else {
-    code.init(rt.environment());
-    code.set_error_handler(&eh);
-    code.attach(&cc);
-
-    for (size_t i = 0; i < count; i++) {
-      code.reinit();
-
-      (void)cc.add_func(FuncSignature::build<uint32_t>());
-      compile_raw_func(cc);
-      cc.end_func();
-      cc.finalize();
-
-      Func fn;
-      rt.add(&fn, &code);
-      rt.release(fn);
+      else if constexpr (op >= CompilerOp::kFinalize) {
+        cc.finalize();
+        if constexpr (op == CompilerOp::kFinalize_RT) {
+          using Func = uint32_t(*)(void);
+          Func fn;
+          rt.add(&fn, &code);
+          rt.release(fn);
+        }
+      }
     }
   }
 }
 #endif // ASMJIT_HAS_HOST_BACKEND && !ASMJIT_NO_COMPILER
 
 template<typename Lambda>
-static inline void test_perf(const char* bench_name, InitStrategy strategy, size_t n, Lambda&& fn) {
+static inline void test_perf(const char* group, const char* params, InitStrategy strategy, size_t n, Lambda&& fn) {
   PerformanceTimer timer;
-  const char* strategy_name = strategy == InitStrategy::kInitReset ? "init/reset" : "reinit    ";
+  const char* strategy_name = strategy == InitStrategy::kInitReset ? "init/reset" : "reinit";
 
   timer.start();
   fn(strategy, n);
   timer.stop();
 
-  printf("%-31s [%s]: %8.3f [ms]\n", bench_name, strategy_name, timer.duration());
+  printf("| %-10s | %-10s | %-23s| %8.1f [ms] |\n", group, strategy_name, params, timer.duration());
 }
 
 static inline void test_perf_all(InitStrategy strategy, size_t n) {
   using IS = InitStrategy;
 
-  test_perf("CodeHolder (Only)"              , strategy, n, [](IS s, size_t n) { bench_codeholder(s, n); });
+  const char frame[]  = "+------------+------------+------------------------+---------------+\n";
+  const char header[] = "| Group      | Strategy   | Features Used          |     Time [ms] |\n";
+
+  printf(frame);
+  printf(header);
+  printf(frame);
+
+  test_perf("CodeHolder", "Reuse Only"          , strategy, n, [](IS s, size_t n) { bench_codeholder(s, n); });
 
 #if defined(ASMJIT_HAS_HOST_BACKEND)
-  test_perf("Assembler"                      , strategy, n, [](IS s, size_t n) { bench_assembler<host::Assembler>(s, n); });
-  test_perf("Assembler + Func"               , strategy, n, [](IS s, size_t n) { bench_assembler_func<host::Assembler>(s, n); });
-  test_perf("Assembler + Func + RT"          , strategy, n, [](IS s, size_t n) { bench_assembler_func_rt<host::Assembler>(s, n); });
+  test_perf("Assembler ", "Reuse Only"          , strategy, n, [](IS s, size_t n) { bench_assembler<host::Assembler>(s, n); });
+  test_perf("Assembler ", "Func"                , strategy, n, [](IS s, size_t n) { bench_assembler_func<host::Assembler, AssemblerOp::kNone>(s, n); });
+  test_perf("Assembler ", "Func + RT"           , strategy, n, [](IS s, size_t n) { bench_assembler_func<host::Assembler, AssemblerOp::kRT>(s, n); });
 #endif
 
 #if defined(ASMJIT_HAS_HOST_BACKEND) && !defined(ASMJIT_NO_BUILDER)
-  test_perf("Builder"                        , strategy, n, [](IS s, size_t n) { bench_builder<host::Builder>(s, n); });
-  test_perf("Builder + Func"                 , strategy, n, [](IS s, size_t n) { bench_builder_func<host::Builder>(s, n, false); });
-  test_perf("Builder + Func + Finalize"      , strategy, n, [](IS s, size_t n) { bench_builder_func<host::Builder>(s, n, true); });
-  test_perf("Builder + Func + Finalize + RT" , strategy, n, [](IS s, size_t n) { bench_builder_func_finalize_rt<host::Builder>(s, n); });
+  test_perf("Builder   ", "Reuse Only"          , strategy, n, [](IS s, size_t n) { bench_builder<host::Builder>(s, n); });
+  test_perf("Builder   ", "Func"                , strategy, n, [](IS s, size_t n) { bench_builder_func<host::Builder, BuilderOp::kNone>(s, n); });
+  test_perf("Builder   ", "Func + Asm"          , strategy, n, [](IS s, size_t n) { bench_builder_func<host::Builder, BuilderOp::kFinalize>(s, n); });
+  test_perf("Builder   ", "Func + Asm + RT"     , strategy, n, [](IS s, size_t n) { bench_builder_func<host::Builder, BuilderOp::kFinalize_RT>(s, n); });
 #endif
 
 #if defined(ASMJIT_HAS_HOST_BACKEND) && !defined(ASMJIT_NO_COMPILER)
 
-  test_perf("Compiler"                       , strategy, n, [](IS s, size_t n) { bench_compiler<host::Compiler>(s, n); });
-  test_perf("Compiler + Func"                , strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler>(s, n, false); });
-  test_perf("Compiler + Func + Finalize"     , strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler>(s, n, true); });
-
-  test_perf("Compiler + Func + Finalize + RT", strategy, n, [](IS s, size_t n) { bench_compiler_func_rt<host::Compiler>(s, n); });
+  test_perf("Compiler  ", "Reuse Only"          , strategy, n, [](IS s, size_t n) { bench_compiler<host::Compiler>(s, n); });
+  test_perf("Compiler  ", "Func"                , strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler, CompilerOp::kNone>(s, n); });
+  test_perf("Compiler  ", "Func + RA"           , strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler, CompilerOp::kCompile>(s, n); });
+  test_perf("Compiler  ", "Func + RA + Asm"     , strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler, CompilerOp::kFinalize>(s, n); });
+  test_perf("Compiler  ", "Func + RA + Asm + RT", strategy, n, [](IS s, size_t n) { bench_compiler_func<host::Compiler, CompilerOp::kFinalize_RT>(s, n); });
 #endif
+
+  printf(frame);
 }
 
 int main(int argc, char* argv[]) {
